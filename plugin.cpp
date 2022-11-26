d18c74e208e412973ada985f3f96df33bebb11ff
Mrowr Purr <mrowr.purr@gmail.com>
2022-11-25 23:20:04 -0800

    Updated for CommonLibSSE-NG

--- plugin.cpp
+++ plugin.cpp
@@ -1,27 +1,11 @@
-#include <SKSE/SKSE.h>
-#include <RE/C/ConsoleLog.h>
+SKSEPluginLoad(const SKSE::LoadInterface* skse) {
+	SKSE::Init(skse);
 
-extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* skse) {
-    SKSE::Init(skse);
-    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* event){
-        if (event->type == SKSE::MessagingInterface::kDataLoaded) {
-            RE::ConsoleLog::GetSingleton()->Print("Hello Skse!");
-        }
-    });
-    return true;
+	// Once all plugins and mods are loaded, then the ~ console is ready and can be printed to
+	SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* message){
+		if (message->type == SKSE::MessagingInterface::kDataLoaded)
+			RE::ConsoleLog::GetSingleton()->Print("Hello, world!");
+	});
+
+	return true;
 }
-
-extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface*, SKSE::PluginInfo* info) {
-    info->infoVersion = SKSE::PluginInfo::kVersion;
-    info->name = "HelloSkse";
-    info->version = 1;
-    return true;
-}
-
-extern "C" __declspec(dllexport) constinit auto SKSEPlugin_Version = [](){
-    SKSE::PluginVersionData version;
-    version.PluginName("HelloSkse");
-    version.PluginVersion({ 0, 0, 1 });
-    version.CompatibleVersions({ SKSE::RUNTIME_LATEST });
-    return version;
-}();
